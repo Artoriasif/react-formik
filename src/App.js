be9014656abc0e4b9e2c772c94f9f5d7083c0dcb
Pedro Mendes <pedrohh0012@icloud.com>
2022-03-28 13:32:25 -0300

    criacao de formulario com Formik e yup

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import schema from "./schema.js";
+
+function App() {
+  function onSubmitMaluco(values, actions) {
+    console.log("Submit", values);
+  }
+
+  return (
+    <div className="App">
+      <Formik
+        validationSchema={schema}
+        onSubmit={onSubmitMaluco}
+        initialValues={{
+          name: "",
+          email: "",
+        }}
+        render={({ values, errors, touched, isvalid }) => (
+          <Form>
+            <div>
+              <label>Nome</label>
+              <Field name="name" type="text" />
+              <ErrorMessage name="name" />
+            </div>
+            <div>
+              <label>Email</label>
+              <Field name="email" type="email" />
+              <ErrorMessage name="email" />
+            </div>
+            <button type="submit" disabled={!isvalid}>
+              Enviar maluco
+            </button>
+          </Form>
+        )}
+      />
+    </div>
+  );
+}
+
+export default App;
